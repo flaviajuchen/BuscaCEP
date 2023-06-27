924a5baecd4c065a64d861df7f569410e299874d
Flavia Juchen <flaviajuchen@gmail.com>
2023-06-27 16:19:41 -0400

    adicionado mapa

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -1,28 +1,36 @@
-import { useState } from 'react';
-import { FiSearch } from 'react-icons/fi';
-import './styles.css';
-import api from './services/api';
+import { useState } from "react";
+import { FiSearch } from "react-icons/fi";
+import "./styles.css";
+import api from "./services/api";
 
 function App() {
-
-  const[input, setInput] = useState('');
+  const [input, setInput] = useState("");
   const [cep, setCep] = useState({});
 
-  async function handleSearch(){
-    if(input === ''){
-      alert("Digite um CEP válido")
+  async function handleSearch() {
+    if (input === "") {
+      alert("Digite um CEP válido");
       return;
     }
 
-    try{
+    try {
       const response = await api.get(`${input}/json`);
       setCep(response.data);
       setInput("");
-    }
-    catch{
+    } catch {
       alert("Erro ao buscar CEP!");
-      setInput("")
+      setInput("");
     }
+  }
+
+  function getMapUrl() {
+    if (cep && cep.cep) {
+      const encodedAddress = encodeURIComponent(
+        `${cep.logradouro}, ${cep.localidade}, ${cep.uf}, ${cep.cep}`
+      );
+      return `https://www.google.com/maps/embed/v1/place?q=${encodedAddress}&key=${process.env.REACT_APP_MAP_KEY}`;
+    }
+    return "";
   }
 
   return (
@@ -30,30 +38,43 @@ function App() {
       <h1 className="title">Busca Cep</h1>
 
       <div className="containerInput">
-        <input 
-        type="text"
-        placeholder="Digite um CEP"
-        value={input}
-        onChange={(e) => setInput(e.target.value) }
+        <input
+          type="text"
+          placeholder="Digite um CEP"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
 
         <button className="buttonSrc" onClick={handleSearch}>
-          <FiSearch size={25} color='#FFF'/>
+          <FiSearch size={25} color="#FFF" />
         </button>
       </div>
 
       {Object.keys(cep).length > 0 && (
-        <main className='main'>
+        <main className="main">
           <h2>CEP: {cep.cep}</h2>
-          <span> {cep.logradouro} </span>
-          <span> {cep.complemento} </span>
-          <span> {cep.bairro} </span>
-          <span> {cep.localidade} - {cep.uf} </span>
-
+          <span>{cep.logradouro}</span>
+          <span>{cep.complemento}</span>
+          <span>{cep.bairro}</span>
+          <span>
+            {cep.localidade} - {cep.uf}
+          </span>
+          <div>
+            {cep && cep.cep && (
+              <iframe
+                title="map"
+                src={getMapUrl()}
+                width="500"
+                height="350"
+                frameBorder="0"
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="0"
+              />
+            )}
+          </div>
         </main>
       )}
-      
-
     </div>
   );
 }
